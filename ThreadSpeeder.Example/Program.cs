cc76d00c5dc10b1e9e04df738d97ccdaa124603f
Gabriel Petrovick <petrovick@hotmail.com>
2019-09-20 10:58:55 -0300

    testing

--- ThreadSpeeder.Example/Program.cs
+++ ThreadSpeeder.Example/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Management;
 using ThreadSpeeder.Example.Application;
 using ThreadSpeeder.Example.Business;
 using ThreadSpeeder.Example.DTO;
@@ -11,10 +13,13 @@ namespace ThreadSpeeder.Example
     {
         static void Main(string[] args)
         {
+            Process proc = Process.GetCurrentProcess();
+            var memoryBeforeCreatingObjects = proc.PrivateMemorySize64;
+
             #region Make objects for testing, don't focus on that :)
             var timeToMakeObjectsBefore = DateTime.Now;
             List<User> users = new List<User>();
-            for(int i = 0; i < 1000; i++)
+            for(int i = 0; i < 10000; i++)
             {
                 users.Add(new User()
                 {
@@ -32,25 +37,59 @@ namespace ThreadSpeeder.Example
             var secondsToMakeObjects = timeToMakeObjectsAfter.Subtract(timeToMakeObjectsBefore);
             #endregion
 
+            var memoryAfterCreatingObjects = proc.PrivateMemorySize64;
+
+
+            PerformanceCounter cpuCounter;
+            PerformanceCounter ramCounter;
+
+            cpuCounter = new PerformanceCounter();
+
+            cpuCounter.CategoryName = "Processor";
+            cpuCounter.CounterName = "% Processor Time";
+            cpuCounter.InstanceName = "_Total";
+
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+
             #region Thread Speeder
             var dateBeforeProcessWithThreadSpeeder = DateTime.Now;
+            var cpuCounterThreadSpeederStart = cpuCounter.NextValue();
+            var ramCounterThreadSpeederStart = ramCounter.NextValue();
             new UserApplication().ProcessWithThreadSpeeder(users);
+            var cpuCounterThreadSpeederFinish = cpuCounter.NextValue();
+            var ramCounterThreadSpeederFinish = ramCounter.NextValue();
             var dateAfterProcessWithThreadSpeeder = DateTime.Now;
             var secondsWithThreadSpeeder = dateAfterProcessWithThreadSpeeder.Subtract(dateBeforeProcessWithThreadSpeeder);
             #endregion
-            
+
+
             #region Without Thread Speeder
             var dateBeforeProcessWithoutThreadSpeeder = DateTime.Now;
+            var cpuCounterWithoutThreadSpeederStart = cpuCounter.NextValue();
+            var ramCounterWithoutThreadSpeederStart = ramCounter.NextValue();
             new UserApplication().ProcessWithoutThreadSpeeder(users);
+            var cpuCounterWithoutThreadSpeederFinish = cpuCounter.NextValue();
+            var ramCounterWithoutThreadSpeederFinish = ramCounter.NextValue();
             var dateAfterProcessWithoutThreadSpeeder = DateTime.Now;
             var secondsWithoutThreadSpeeder = dateAfterProcessWithoutThreadSpeeder.Subtract(dateBeforeProcessWithoutThreadSpeeder);
             #endregion
+
+            var processorname = "";
+            ManagementObjectSearcher mos =
+  new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Processor");
+            foreach (ManagementObject mo in mos.Get())
+            {
+                processorname = mo["Name"].ToString();
+            }
+
             
+            Console.WriteLine(String.Format("|{0,20}-{1,50}-{2,15}-{3,20}-{4,15}-{5,16}-{6,15}|", "--------------------", "--------------------------------------------------", "---------------", "--------------------", "---------------", "---------------", "---------------"));
+            Console.WriteLine(String.Format("|{0,20}|{1,50}|{2,15}|{3,20}|{4,15}|{5,16}|{6,15}|", "Descrição", "CPU", "Total Memory", "CPU Usage(%)", "RAM(Mbs)", "Time", "Nº Total List"));
+            Console.WriteLine(String.Format("|{0,20}|{1,50}|{2,15}|{3,20}|{4,15}|{5,16}|{6,15}|", "No ThreadSpeeder", processorname, "16GB", (cpuCounterWithoutThreadSpeederFinish - cpuCounterWithoutThreadSpeederStart), (ramCounterWithoutThreadSpeederFinish - ramCounterWithoutThreadSpeederStart), secondsWithoutThreadSpeeder.ToString(), users.Count));
+            Console.WriteLine(String.Format("|{0,20}|{1,50}|{2,15}|{3,20}|{4,15}|{5,16}|{6,15}|", "ThreadSpeeder", processorname, "16GB", (cpuCounterThreadSpeederFinish - cpuCounterThreadSpeederStart), (ramCounterThreadSpeederFinish - ramCounterThreadSpeederStart), secondsWithThreadSpeeder.ToString(), users.Count));
+            Console.WriteLine(String.Format("|{0,20}-{1,50}-{2,15}-{3,20}-{4,15}-{5,16}-{6,15}|", "--------------------", "--------------------------------------------------", "---------------", "--------------------", "---------------", "---------------", "---------------"));
 
-            Console.WriteLine("it took to create the objets(that does'nt mean nothing to us, don't worry: " + secondsToMakeObjects.ToString());
-            Console.WriteLine("With ThreadSpeeder.Core it took: " + secondsWithThreadSpeeder.ToString());
-            Console.WriteLine("Without ThreadSpeeder.Core it took: " + secondsWithoutThreadSpeeder.ToString());
-
+            Console.WriteLine("");
             Console.ReadLine();
 
 
